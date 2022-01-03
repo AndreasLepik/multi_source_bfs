06bd2c9981fea64df86bca5c3af035596f8a61d7
Andreas <an8300le-s@student.lu.se>
2022-01-03 17:12:20 +0100

    Tab separated values reading and int converting.

--- src/graph.cc
+++ src/graph.cc
@@ -1,20 +1,34 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include "graph.h"
 using namespace std;
 
-void Graph::loadGraphFromFile(string inputFilePath) {
+void Graph::loadGraphFromFile(string inputFilePath)
+{
   cout << "Loading graph from file..." << endl;
-  ifstream input (inputFilePath);
+  ifstream input(inputFilePath);
   string line;
-  while (input) {
-    getline(input, line);
-    cout << line << endl;
+
+  // First line is just meta info
+  getline(input, line);
+
+  while (getline(input, line))
+  {
+    istringstream stream(line);
+    string first, second;
+    getline(stream, first, '\t');
+    getline(stream, second);
+    int a = stoi(first);
+    int b = stoi(second);
+    cout << a << endl;
+    cout << b << endl;
   }
   this->adjecency_data.insert(make_pair<int, vector<int>>(1, {1, 2, 3}));
 }
 
-vector<int> Graph::getEdges(int node) {
+vector<int> Graph::getEdges(int node)
+{
   return this->adjecency_data.at(node);
 }
