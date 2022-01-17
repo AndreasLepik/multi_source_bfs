bfc55f114471f74eef6770646a94207d8e0e742e
Andreas <an8300le-s@student.lu.se>
2022-01-17 14:51:06 +0100

    Added timer to run.

--- src/context.cc
+++ src/context.cc
@@ -1,3 +1,5 @@
+#include <ctime>
+#include <iostream>
 #include "context.h"
 #include "graph.h"
 
@@ -8,12 +10,27 @@ Context::Context(string path, int size, BFS *algorithm, bool timer)
 
 void Context::run(vector<int> sources)
 {
-  bfs->run(graph, sources);
+  if (timer)
+  {
+    auto start = clock();
+    bfs->run(graph, sources);
+    auto end = clock();
+    cout << "Time: " << (end - start) / (double)( CLOCKS_PER_SEC / 1000 ) << "ms" << endl;
+  }
+  else
+  {
+    bfs->run(graph, sources);
+  }
 }
 
 void Context::setData(string path, int graphSize)
 {
   this->graph = Graph{graphSize, path};
+}
+
+void Context::setTimer(bool timer)
+{
+  this->timer = timer;
 }
 
 void Context::setAlgorithm(BFS *algorithm)
